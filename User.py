96da3a46cdff17e9f3b4aacac2f3f22977a34580
Zhaoshan-Duan <joshua.duan47@gmail.com>
2023-12-01 15:59:07 -0800

    added simple log in with given passwords

---
+++ User.py
@@ -0,0 +1,12 @@
+class User:
+
+    def __init__(self, username, password):
+        self._username = username
+        self._password = password
+
+    def get_username(self):
+        return self._username
+    
+    def get_password(self):
+        return self._password
+    
