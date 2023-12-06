e4ea9fda9438f5edd4087e84a3371f30a8a766b8
Zhaoshan-Duan <joshua.duan47@gmail.com>
2023-12-05 22:12:18 -0800

    testing streamlit

---
+++ test_streamlit.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import openai
+import config
+
+with st.chat_message("user"):
+    st.write("hi")
+
+# with st.sidebar:
+#     st.title('🤖💬 OpenAI Chatbot')
+#     openai.api_key = config.api_key 
+
+# if "messages" not in st.session_state:
+#     st.session_state.messages = []
+
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
+
+# if prompt := st.chat_input("What is up?"):
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+#     with st.chat_message("user"):
+#         st.markdown(prompt)
+#     with st.chat_message("assistant"):
+#         message_placeholder = st.empty()
+#         full_response = ""
+#         for response in openai.ChatCompletion.create(
+#             model="gpt-3.5-turbo",
+#             messages=[{"role": m["role"], "content": m["content"]}
+#                       for m in st.session_state.messages], stream=True):
+#             full_response += response.choices[0].delta.get("content", "")
+#             message_placeholder.markdown(full_response + "▌")
+#         message_placeholder.markdown(full_response)
+#     st.session_state.messages.append({"role": "assistant", "content": full_response})
